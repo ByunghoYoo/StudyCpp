02458f61b3fe8b8a994e3e08f116f459932f292b
ByunghoYoo <66407407+ByunghoYoo@users.noreply.github.com>
2020-10-21 23:53:41 +0900

    20/10/21 23:53 commit

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,9 +1,13 @@
-// HelloWorld.c
+#include <iostream>
 
-#include <stdio.h>
-
-int main() {
-	printf("Hello World! \n");
-
+int main(void)
+{
+	int num = 20;
+	// std::cout<<'출력대상1'<<'출력대상2<<..;
+	// std::endl 이후에 줄 바꿈이 일어난다.
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Hello " << "World!" << std::endl;
+	std::cout << num << ' ' << 'A';
+	std::cout << ' ' << 3.14 << std::endl;
 	return 0;
 }
